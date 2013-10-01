5515eebbcb69079c59f910fffdf3febcd0c47594
Chuka Okoye <okoye9@gmail.com>
2013-10-01 11:43:11 -0700

    added twoplaces.go and ex2

---
+++ ex1/twoplaces.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  var pi = 3.14159
+  var sun_temperature = 5780.0
+  fmt.Printf("%2.2f\n", pi)
+  fmt.Printf("%e\n", pi)
+  fmt.Printf("%g\n", pi)
+  fmt.Printf("%2f",sun_temperature)
+}
